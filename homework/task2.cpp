322b11f0f402bf8c0db70e94701673d7996a4a4f
ArsenyCoder <114302368+ArsenyCoder@users.noreply.github.com>
2022-12-29 12:41:17 +0300

    Add files via upload

---
+++ homework/task2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int argv(int *array,int n) {
+    int sum = 0;
+
+    for (int i = 0; i < n; i++) {
+        sum += array[i];
+    }
+
+    return sum / n;
+}
+
+
+void main()
+{
+    int array[5] = {1,2,3,4,5};
+    //3
+
+    cout << argv(array, sizeof(array) / sizeof(array[0]));
+
+    
+}
